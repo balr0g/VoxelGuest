bd72bc559263aced8d24a6f485b4c2b9ef97ee1d
psanker <patricksanker@gmail.com>
2012-02-23 15:32:48 -0700

    Added GPL v3 header

--- VoxelGuest/src/main/java/com/thevoxelbox/commands/Command.java
+++ VoxelGuest/src/main/java/com/thevoxelbox/commands/Command.java
@@ -1,17 +1,28 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * VoxelGuest
+ *
+ * Copyright (C) 2011, 2012 psanker and contributors
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
+
 package com.thevoxelbox.commands;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- *
- * @author patrick
  * 
  * Inspired by sk89q's command system
+ *
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
--- VoxelGuest/src/main/java/com/thevoxelbox/voxelguest/util/FlatFileManager.java
+++ VoxelGuest/src/main/java/com/thevoxelbox/voxelguest/util/FlatFileManager.java
@@ -1,3 +1,19 @@
+/*
+ * VoxelGuest
+ *
+ * Copyright (C) 2011, 2012 psanker and contributors
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package com.thevoxelbox.voxelguest.util;
 
 import java.io.File;
@@ -5,8 +21,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class FlatFileManager {
     
